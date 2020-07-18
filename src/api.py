50192ab6168819945c228958d828e986ef43a9ec
wnajsldkf <wnajsldkf@wnajsldkfui-MacBookAir.local>
2020-07-18 16:57:15 +0900

    edit : api.py

--- src/api.py
+++ src/api.py
@@ -18,23 +18,29 @@ def signup():
 def index():
     if 'id' in session:
         return ('이미 로그인된 상태입니다.')
-    return ('로그인 되지 않았습니다.')  
+    else:
+        return ('로그인 되지 않았습니다.')  
 
-@app.route('/user/signin',methods=['POST'])
+@app.route('/signin',methods=['POST'])
 def signin():
     try:
+        # 로그인 성공
         session['id'] = 'wnajsldkf'
-        return {'StatusCode' : 200, 'message':'User login in completed'}
+        return {'StatusCode' : 200, 'message':'User login in completed.'}
     except:
-        return {'StatusCode' : 400, 'message':'User login is failed'}
-
+        # 잘못된 비밀번호
+        return {'StatusCode' : 409, 'message':'You entered wrong password.'}
+        # 아이디가 없을 때
+    else:
+        return {'StatusCode' : 404, 'message':'There is no ID'}
+    
 @app.route('/user/signout')
 def signout():
     try:    
         session.pop('id',None)
         return {'StatusCode' : 200, 'message' : 'User Logout is completed'}
     except:
-        return {'StatusCode' : 400, 'message' : 'User Logout is failed'}
+        return {'StatusCode' : 400, 'message' : 'This ID had not been logged in'}
 
 if __name__ == '__main__':
     app.run(debug=True)
